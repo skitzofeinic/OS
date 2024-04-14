3e7514addb4f5f20a887d8960776dbddcbc82706
anh <anhle261137@gmail.com>
2024-04-14 19:02:26 +0200

    signals wip

--- 1-shell/shell.c
+++ 1-shell/shell.c
@@ -7,6 +7,7 @@
 #include "front.h"
 #include "parser/ast.h"
 #include "shell.h"
+#include <signal.h>
 
 void my_free_tree(void *pt)
 {
@@ -16,6 +17,10 @@ void my_free_tree(void *pt)
 void initialize(void)
 {
     // This code will be called once at startup
+    if (signal(SIGINT, SIG_IGN) == SIG_ERR) {
+        perror("signal");
+        exit(EXIT_FAILURE);
+    }
 }
 
 void shell_exit(void)
@@ -69,8 +74,56 @@ void execute_single_command(node_t *node) {
     }
 }
 
-void run_command(node_t *node)
-{
+void execute_piped_commands(node_t *first_command, node_t *second_command) {
+    int pipefd[2];
+    pid_t pid1, pid2;
+    int status;
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid1 = fork();
+    if (pid1 == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid1 == 0) {
+        // Child process for the first command
+        close(pipefd[0]); // Close unused read end of the pipe
+
+        dup2(pipefd[1], STDOUT_FILENO); // Redirect stdout to the write end of the pipe
+        close(pipefd[1]); // Close write end after redirection
+
+        run_command(first_command); // Execute the first command
+        exit(EXIT_SUCCESS);
+    }
+
+    pid2 = fork();
+    if (pid2 == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid2 == 0) {
+        // Child process for the second command
+        close(pipefd[1]); // Close unused write end of the pipe
+
+        dup2(pipefd[0], STDIN_FILENO); // Redirect stdin to the read end of the pipe
+        close(pipefd[0]); // Close read end after redirection
+
+        run_command(second_command); // Execute the second command
+        exit(EXIT_SUCCESS);
+    }
+
+    // Parent process
+    close(pipefd[0]); // Close unused read end of the pipe
+    close(pipefd[1]); // Close unused write end of the pipe
+
+    // Wait for both child processes to complete
+    waitpid(pid1, &status, 0);
+    waitpid(pid2, &status, 0);
+}
+
+void run_command(node_t *node) {
     arena_push(); // Create a new memory arena
 
     if (node == NULL) {
@@ -83,6 +136,8 @@ void run_command(node_t *node)
     } else if (node->type == NODE_SEQUENCE) {
         run_command(node->sequence.first);
         run_command(node->sequence.second);
+    } else if (node->type == NODE_PIPE) {
+        execute_piped_commands(node->pipe.parts[0], node->pipe.parts[1]);
     }
 
     arena_pop(); // Clean up memory arena
