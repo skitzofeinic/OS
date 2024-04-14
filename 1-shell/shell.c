e18db84814e595e110b035aa8476d1703971e977
anh <anhle261137@gmail.com>
2024-04-14 18:40:20 +0200

    pipes wip

--- 1-shell/shell.c
+++ 1-shell/shell.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #include "arena.h"
 #include "front.h"
 #include "parser/ast.h"
@@ -5,33 +10,80 @@
 
 void my_free_tree(void *pt)
 {
-	free_tree((node_t *)pt);
+    free_tree((node_t *)pt);
 }
 
 void initialize(void)
 {
-	// This code will be called once at startup
-
-
+    // This code will be called once at startup
 }
 
 void shell_exit(void)
 {
-    // This code will be called on exit,
-    // this might not be required but could be useful
+    // This code will be called on exit
 }
 
+void execute_single_command(node_t *node) {
+    if (node == NULL || node->type != NODE_COMMAND)
+        return;
+
+    char *program = node->command.program;
+    char **argv = node->command.argv;
+
+    // Handle built-in commands
+    if (strcmp(program, "exit") == 0) {
+        int exit_code = 0;
+        if (argv[1] != NULL)
+            exit_code = atoi(argv[1]);
+        exit(exit_code);
+    }
+
+    if (strcmp(program, "cd") == 0) {
+        if (argv[1] != NULL) {
+            if (chdir(argv[1]) == -1)
+                perror("cd");
+        } else {
+            fprintf(stderr, "cd: missing argument\n");
+        }
+    } else {
+        // External command execution
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Child process
+            execvp(program, argv);
+            // execvp returns only if an error occurs
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        }
+
+        if (pid > 0) {
+            // Parent process
+            int status;
+            waitpid(pid, &status, 0);
+            if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+                fprintf(stderr, "%s: command failed with exit status %d\n", program, WEXITSTATUS(status));
+        } else {
+            // Fork failed
+            perror("fork");
+        }
+    }
+}
 
 void run_command(node_t *node)
 {
-	// Create a new memory arena to handle this function in. Please see the
-	// hints and `arena.h` on how to use them, as you can very easily create
-	// memory leaks if you are not careful.
-	arena_push();
+    arena_push(); // Create a new memory arena
 
-	// (for testing:)
-	print_tree(node);
+    if (node == NULL) {
+        arena_pop(); // Clean up memory arena
+        return;
+    }
 
-	// Pop the current memory arena, this frees all memory in the current arena.
-	arena_pop();
+    if (node->type == NODE_COMMAND) {
+        execute_single_command(node);
+    } else if (node->type == NODE_SEQUENCE) {
+        run_command(node->sequence.first);
+        run_command(node->sequence.second);
+    }
+
+    arena_pop(); // Clean up memory arena
 }
