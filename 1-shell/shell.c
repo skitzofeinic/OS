d192e3aeb13280e847657ad7a9022b552d7952a2
anh <anhle261137@gmail.com>
2024-04-14 21:21:47 +0200

    advanced pipes 0.66

--- 1-shell/shell.c
+++ 1-shell/shell.c
@@ -66,68 +66,71 @@ void execute_single_command(node_t *node) {
     }
 }
 
-void execute_piped_commands(node_t *first_command, node_t *second_command) {
-    int pipefd[2];
-    pid_t pid1, pid2;
-    int status;
-
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-
-    pid1 = fork();
-    if (pid1 == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid1 == 0) {
-        // Child process for the first command
-        close(pipefd[0]); // Close unused read end of the pipe
-        dup2(pipefd[1], STDOUT_FILENO); // Redirect stdout to the write end of the pipe
-        close(pipefd[1]); // Close write end after redirection
-        run_command(first_command); // Execute the first command
-        exit(EXIT_SUCCESS);
-    }
-
-    pid2 = fork();
-    if (pid2 == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    } else if (pid2 == 0) {
-        // Child process for the second command
-        close(pipefd[1]); // Close unused write end of the pipe
-        dup2(pipefd[0], STDIN_FILENO); // Redirect stdin to the read end of the pipe
-        close(pipefd[0]); // Close read end after redirection
-        run_command(second_command); // Execute the second command
-        exit(EXIT_SUCCESS);
-    }
-
-    // Parent process
-    close(pipefd[0]); // Close unused read end of the pipe
-    close(pipefd[1]); // Close unused write end of the pipe
-
-    // Wait for both child processes to complete
-    waitpid(pid1, &status, 0);
-    waitpid(pid2, &status, 0);
-}
-
-
 void run_command(node_t *node) {
-    arena_push();
+    arena_push(); // Create a new memory arena
 
     if (node == NULL) {
-        arena_pop();
+        arena_pop(); // Clean up memory arena
         return;
     }
 
     if (node->type == NODE_COMMAND) {
         execute_single_command(node);
-    } else if (node->type == NODE_SEQUENCE) {
+    }
+    
+    if (node->type == NODE_SEQUENCE) {
         run_command(node->sequence.first);
         run_command(node->sequence.second);
-    } else if (node->type == NODE_PIPE) {
-        execute_piped_commands(node->pipe.parts[0], node->pipe.parts[1]);
+    }
+    
+    if (node->type == NODE_PIPE) {
+        int num_commands = node->pipe.n_parts;
+        int pipefd[num_commands - 1][2];
+        pid_t pids[num_commands];
+        int status;
+
+        for (int i = 0; i < num_commands - 1; i++) {
+            if (pipe(pipefd[i]) == -1) {
+                perror("pipe");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        for (int i = 0; i < num_commands; i++) {
+            pids[i] = fork();
+            if (pids[i] == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else if (pids[i] == 0) {
+                // Child process
+                if (i != 0) {
+                    dup2(pipefd[i - 1][0], STDIN_FILENO); // Redirect stdin from previous pipe
+                    close(pipefd[i - 1][0]); // Close read end of previous pipe
+                }
+                if (i != num_commands - 1) {
+                    dup2(pipefd[i][1], STDOUT_FILENO); // Redirect stdout to next pipe
+                    close(pipefd[i][1]); // Close write end of current pipe
+                }
+                for (int j = 0; j < num_commands - 1; j++) {
+                    close(pipefd[j][0]); // Close read ends of all other pipes
+                    close(pipefd[j][1]); // Close write ends of all other pipes
+                }
+                run_command(node->pipe.parts[i]);
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+        // Parent process
+        for (int i = 0; i < num_commands - 1; i++) {
+            close(pipefd[i][0]); // Close read ends of all pipes
+            close(pipefd[i][1]); // Close write ends of all pipes
+        }
+
+        // Wait for all child processes to complete
+        for (int i = 0; i < num_commands; i++) {
+            waitpid(pids[i], &status, 0);
+        }
     }
 
-    arena_pop();
+    arena_pop(); // Clean up memory arena
 }
